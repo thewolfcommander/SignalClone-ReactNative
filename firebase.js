843a8c8504956b38256a44f0e5b3d3410a797ce4
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-06-27 17:21:16 +0530

    Installed React Native Navigation and also created Firebase config

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAQlfeDbtVPEvbMoGE6Kf2evUCpwy87K4g",
+  authDomain: "signalclone-5b3ac.firebaseapp.com",
+  projectId: "signalclone-5b3ac",
+  storageBucket: "signalclone-5b3ac.appspot.com",
+  messagingSenderId: "742631831387",
+  appId: "1:742631831387:web:a38c06e2eaf2d05b7a4c7e",
+};
