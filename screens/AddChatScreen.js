ce82c728eaef8cf02b8bcadaa259045130bbb616
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-06-28 20:26:51 +0530

    Completed Add to Chat Screen

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,20 +1,37 @@
 import React, { useLayoutEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { db } from "../firebase";
 
 const AddChatScreen = ({ navigation }) => {
   const [chatName, setChatName] = useState("");
 
-  const createChat = () => {
-    alert(chatName);
+  const createChat = async () => {
+    if (chatName === "") {
+      alert("you have to provide the chat name");
+    } else {
+      await db
+        .collection("chats")
+        .add({
+          chatName: chatName,
+        })
+        .then(() => {
+          navigation.goBack();
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    }
   };
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: "Add Chat",
+      title: "Add a new Chat",
       headerStyle: { backgroundColor: "#fff" },
       headerTitleStyle: { color: "black" },
       headerTintColor: "black",
+      headerBackTitle: "Chats",
     });
   }, [navigation]);
   return (
@@ -30,13 +47,23 @@ const AddChatScreen = ({ navigation }) => {
           value={chatName}
           onChangeText={(text) => setChatName(text)}
           type="text"
+          onSubmitEditing={createChat}
+          leftIcon={
+            <Icon
+              name="wechat"
+              type="antdesign"
+              size={24}
+              color="black"
+              style={{ marginRight: 10 }}
+            />
+          }
         />
       </View>
       <Button
         containerStyle={styles.button}
         onPress={createChat}
         raised
-        title="Create Chat"
+        title="Create new Chat"
       />
     </View>
   );
@@ -46,8 +73,10 @@ export default AddChatScreen;
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "white",
+    height: "100%",
+    padding: 30,
   },
   inputContainer: {
     width: 300,
